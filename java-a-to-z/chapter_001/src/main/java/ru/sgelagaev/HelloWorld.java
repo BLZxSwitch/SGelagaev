705f6e275b58b1b2290144b975e744f15fc85bab
Sergey Gelagaev <BLZ-Switch@yandex.ru>
2017-08-07 19:08:10 +0300

    1.3. JavaDoc. Code convention [#4087]

--- java-a-to-z/chapter_001/src/main/java/ru/sgelagaev/HelloWorld.java
+++ java-a-to-z/chapter_001/src/main/java/ru/sgelagaev/HelloWorld.java
@@ -1,6 +1,15 @@
 package ru.sgelagaev;
 
+//**
+ *Class Клас для вывода приведственного сообщения на экран
+ *@author sgelagaev
+ *@since 07.08.2017
+ *@version 1
+ */
 public class HelloWorld{
+	/**Метод main, вывод сообщения на экран
+	*@param args параметры командной строки, не используются
+	*/
 	public static void main(String[] args){
 		System.out.println("Hellow World!");
 	}
---
+++ java-a-to-z/chapter_001/src/main/java/ru/sgelagaev/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Sergey Gelagaev (mailto:blz-switch@ya.ru)
+* @version $Id$
+* @since 07.08.2017
+*/
+package ru.sgelagaev;
